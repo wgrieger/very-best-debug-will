08ae0186839546cfd372035b0b406d6ba2141c61
Will Grieger <165510301+wgrieger@users.noreply.github.com>
2024-05-19 00:59:34 +0000

    iunderway

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
 
+
+  def all_users
+  render({:template=>"user_templates/all_users"})
+  end
+
   def index
     matching_users = User.all
     @users = matching_users.order(:created_at)
 
-    render({ :template => "users_templates/all_users"})
+    render({ :template => "user_templates/all_users"})
   end
   
   def show
